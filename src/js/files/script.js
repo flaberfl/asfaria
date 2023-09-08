364b5b68737317114e5915b84bf71df923cbb753
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-09-08 16:34:30 +0300

    home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -497,9 +497,9 @@ quizApartments();
 
 let card = document.querySelector('.types__image_img');
 
-document.addEventListener('mousemove', function (e) {
-  let xAxis = (window.innerWidth / 3 - e.pageX);
-  let yAxis = (window.innerHeight / 3 - e.pageY);
+card.addEventListener('mousemove', function (e) {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 5;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 
