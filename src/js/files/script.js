e39f6e7b58537c035ebc33d87fe017295f770a49
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-09-08 22:47:03 +0300

    home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -492,19 +492,6 @@ quizYachts();
 quizOffices();
 quizApartments();
 
-
-
-
-let card = document.querySelector('.types__image_img');
-
-card.addEventListener('mousemove', function (e) {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 5;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-
-
-
 // document.addEventListener("afterPopupOpen", function (e) {
 //   // Попап
 //   const currentPopup = e.detail.popup;
